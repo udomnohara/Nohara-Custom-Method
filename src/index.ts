d630798649e49cc7ff950023d3aecd5cae524f01
unknown <oudom.ever@gmail.com>
2023-02-15 19:31:22 +0700

    add space number every night

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-
-//valid if date is in toISOString() format
+// valid if date is in toISOString() format
 export const isValidIsoDate = (date: string | undefined): boolean => {
   if (!date) return false;
   try{
@@ -8,4 +7,36 @@ export const isValidIsoDate = (date: string | undefined): boolean => {
   }catch (e) {
     return false;
   }
+}
+
+/**
+ * space string in every n digit
+ * @param input string to be spaced
+ * @param n number of digit
+ * @param isSkipLastNPlusOneDigit if true, it will skip last n+1 digit
+ * @returns spaced string
+ * @example
+ * 1234567890 => 123 456 789 0 (n = 3, isSkipLastNPlusOneDigit = false) or 123 456 7890 (n = 3, isSkipLastNPlusOneDigit = true)
+ */
+export function spaceNumberEveryNDigit(
+  input: string,
+  n: number,
+  isSkipLastNPlusOneDigit = false
+) {
+  if (!n) return "";
+  const numberArray = input.split("");
+  const result: string[] = [];
+  for (let i = 0; i < numberArray.length; i++) {
+    if (isSkipLastNPlusOneDigit) {
+      if (i % n === 0 && i !== 0 && i !== numberArray.length - 1) {
+        result.push(" ");
+      }
+    } else {
+      if (i % n === 0 && i !== 0) {
+        result.push(" ");
+      }
+    }
+    result.push(numberArray[i]);
+  }
+  return result.join("");
 }
--- test/test.ts
+++ test/test.ts
@@ -1,13 +1,14 @@
-import { isValidIsoDate } from '../src';
+import { isValidIsoDate, spaceNumberEveryNDigit } from '../src';
 import { assert } from 'chai';
 import { describe, it } from "mocha";
-//using mocha testing framework
+// using mocha testing framework
 
-//testing isValidIsoDate
 describe('My test', () => {
+// test isValidIsoDate
   it('isValidIsoDate', () => {
     assert.isFalse(isValidIsoDate(''))
     assert.isFalse(isValidIsoDate(undefined))
+    assert.isFalse(isValidIsoDate('2020-10-15'))
     assert.isTrue(isValidIsoDate('2020-10-15T00:00:00.000Z'))
   })
 })
